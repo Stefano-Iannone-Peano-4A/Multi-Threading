b672cdbabf6c3e383adf34bb2d6d8a6e4b207889
Stefano-Iannone-Peano-4A <56471045+Stefano-Iannone-Peano-4A@users.noreply.github.com>
2019-11-09 10:58:43 +0100

    Thread con classe Say

---
+++ TPS 09-11-19/src/tps/pkg09/pkg11/pkg19/Hello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tps.pkg09.pkg11.pkg19;
+
+/**
+ *
+ * @author falsetti.francesco-iannone.stefano
+ */
+public class Hello extends Thread {
+
+    public void run() {
+        for (int i = 0; i < 8; i++) {
+            System.out.println("hello");
+        }
+
+    }
+}
---
+++ TPS 09-11-19/src/tps/pkg09/pkg11/pkg19/TPS091119.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tps.pkg09.pkg11.pkg19;
+
+/**
+ *
+ * @author falsetti.francesco-iannone.stefano
+ */
+public class TPS091119 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Hi w1=new Hi();
+        Hello w2=new Hello();
+        w1.start();
+        w2.start();*/
+
+        Say w3 = new Say("ciao");
+        Say w4 = new Say("ciao1");
+        Say w5 = new Say("ciao2");
+        w3.start();
+        w4.start();
+        w5.start();
+
+    }
+
+}
